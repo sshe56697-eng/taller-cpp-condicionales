3568d80f3bf5142d7a3c8118f9d8d7a69010be68
Samuel Moreno <sshe56697@gmail.com>
2026-03-03 21:00:11 -0500

    Prueba 3

--- ejercicio_01/ejercicio_01.cpp
+++ ejercicio_01/ejercicio_01.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <iomanip>
 int main () {
 double temperature;
-std::<<"Enter C temperature"
-scin::>> temperature;
+std::cout <<"Enter Celsius temperature: ";
+std::cin>> temperature;
+double F = temperature * 9/5 + 32;
+double K = temperature + 273.15;
+double R = (temperature + 273.15) * 9/5;
+
+std::cout << std::fixed << std::setprecision(2);
+std::cout << "Equals to: " << F << "° Fahrenheit" << std::endl;
+std::cout << "Equals to: " << K << "Kelvin" << std::endl;
+std::cout << "Equals to: " << R << "° Rankine" << std::endl;
 
 
-
-    return 0;
+    return 0;   
 }
